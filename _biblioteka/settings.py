3b00b6f1a404bd6f20ee0285ed88f9f742a8f1df
Josiel Luz <josiel9511791722@gmail.com>
2023-03-06 14:49:20 -0300

    initial configs

--- _biblioteka/settings.py
+++ _biblioteka/settings.py
@@ -88,6 +88,8 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": 5432,
     }
 }
 
