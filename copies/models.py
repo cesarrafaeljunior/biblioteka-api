1de05c08aad11623310f6c2799299e7768cb9209
Josiel Luz <josiel9511791722@gmail.com>
2023-03-08 16:15:19 -0300

    feat: add loan models and serializers

--- copies/models.py
+++ copies/models.py
@@ -1,22 +1,31 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
 class Copy(models.Model):
     ammounts_of_copies = models.IntegerField(
-        validators=[
-            MinValueValidator(0),
-            MaxValueValidator(1000)
-        ]
+        validators=[MinValueValidator(0), MaxValueValidator(1000)]
     )
     copies_avaliable = models.IntegerField(
-        validators=[
-            MinValueValidator(0),
-            MaxValueValidator(1000)
-        ],
+        validators=[MinValueValidator(0), MaxValueValidator(1000)],
     )
 
     book = models.ForeignKey(
-        "books.Book",
-        on_delete=models.CASCADE,
-        related_name="copies"
+        "books.Book", on_delete=models.CASCADE, related_name="copies"
+    )
+    users_loan = models.ManyToManyField(
+        "users.User", through="copies.Loan", related_name="copies_loan"
+    )
+
+
+class Loan(models.Model):
+    date_receipt = models.DateField(auto_now_add=True)
+    date_devolution = models.DateField()
+    is_receipt = models.BooleanField(default=False)
+    price = models.IntegerField()
+    copy = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="copy_borrowed"
+    )
+    user = models.ForeignKey(
+        "copies.Copy", on_delete=models.CASCADE, related_name="user_copy_borrowed"
     )
